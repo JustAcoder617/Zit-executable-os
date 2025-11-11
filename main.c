a7b413a7c2fd0c691a65f00f8429bbd7e50a080a
JustAcoder617 <antonioalvesdasilvaalysson0@Gmail.com>
2025-11-11 19:49:47 -0300

    system initial

--- main.c
+++ main.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(void){
-    puts("Hello! welcome to the Ziper Os.exe! wanna see our documentation? go to www.github.com/JustAcoder617/Zit-executable-os.");
-    puts("We are working to prepare our system!");
-    load();
+#include <windows.h>
+
+int chances = 3;
+
+void password_check(FILE *file) {
+    char password[100];
+    char stored_password[100];
+
+    fgets(stored_password, sizeof(stored_password), file);
+    stored_password[strcspn(stored_password, "\n")] = 0; // remove \n
+
+    while (chances > 0) {
+        printf("Please type your password: ");
+        fgets(password, sizeof(password), stdin);
+        password[strcspn(password, "\n")] = 0;
+
+        if (strcmp(password, stored_password) == 0) {
+            printf("Access granted!\n");
+            return;
+        } else {
+            chances--;
+            printf("Incorrect password. %d more tries.\n", chances);
+        }
+    }
+
+    printf("0 chances remaining.\n");
+    Sleep(6000);
+    printf("Quitting the system...\n");
+    Sleep(3000);
+    exit(0);
 }
-void load(){
+
+void load() {
     FILE *file = fopen("password.txt", "r");
     if (file == NULL) {
-        fprintf(stderr, "404p");
-        char error='1';
-        maintenance_mode(error); //404 password and redirect to maintenance mode
+        printf("Error: password file not found. Entering maintenance mode...\n");
+        exit(1);
     }
     password_check(file);
-    // Read and process the file contents here
     fclose(file);
+}
+
+int main(void) {
+    puts("Hello! Welcome to Ziper OS.exe!");
+    puts("Documentation: www.github.com/JustAcoder617/Zit-executable-os");
+    puts("Preparing the system...");
+    load();
 }
